81d130bb20922b035bc296a19eacddd37a76865f
Lucyna Mazur <lucyna@studiokreator.com>
2018-11-03 22:36:16 +0100

    scroll reveal and smooth scrolling included

--- webpage/static/webpage/js/common.js
+++ webpage/static/webpage/js/common.js
@@ -315,4 +315,36 @@ $(document).ready(function() {
 
     /* autocomplete -stop */
 
+
+    /* smooth scrolling - start */
+    function smoothScrolling() {
+        $(".navbar a").on('click', function(event) {
+
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+
+                // Store hash
+                var hash = this.hash;
+
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+                });
+            } // End if
+        });
+    }
+    smoothScrolling();
+    /* smooth scrolling - stop */
+
+    /*scrollReveal - start*/
+    window.sr = ScrollReveal({ reset: true }).reveal('.scroll', { duration: 500 });
+    /*scrollReveal - stop*/
+
 });
